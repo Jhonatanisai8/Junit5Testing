00207ef26be00141b06513a09a7b13f72003eb67
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-02-12 09:35:13 -0500

    test para el saldo de la cuenta, ademas de que no puede ser negativo

--- src/test/java/com/jhonatan/junitapp/ejemplo/models/CuentaTest.java
+++ src/test/java/com/jhonatan/junitapp/ejemplo/models/CuentaTest.java
@@ -13,12 +13,21 @@ class CuentaTest {
      * prueba que se pase un propietario a la cuenta
      */
     @Test
-    void testNombreCuenta() {
+    void testPropietarioCuenta() {
         Cuenta cuenta = new Cuenta("JhonSai", new BigDecimal("1000.12345"));
         //cuenta.setPropietario("JhonSai");
         String esperado = "JhonSai";
         String actual = cuenta.getPropietario();
         assertEquals(esperado, actual);
         assertTrue(esperado.equals(actual));
+    }
+
+    @Test
+    void testSaldoCuenta() {
+        Cuenta cuenta = new Cuenta("JhonSai", new BigDecimal("1000.12345"));
+        assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
+        int numero = cuenta.getSaldo().compareTo(BigDecimal.ZERO);
+        assertFalse(numero < 0); //si es mayor el numero a cero el saldo es negativo
+        assertTrue(numero > 0);//si es mayor que 0, el saldo es positivo
     }
 }
