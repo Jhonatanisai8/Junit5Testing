39c61b30cf82d5bbfdb6a1132324a0285225ea98
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-14 01:29:00 -0500

    clase DividirPorCeroException para validar que no se divida por cero

---
+++ src/main/java/com/jhonatan/junitapp/ejemplo/exceptions/DividirPorCeroException.java
@@ -0,0 +1,7 @@
+package com.jhonatan.junitapp.ejemplo.exceptions;
+
+public class DividirPorCeroException extends Exception {
+    public DividirPorCeroException(String message) {
+        super(message);
+    }
+}
