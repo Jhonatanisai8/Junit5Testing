594ec0158346f4b5839e90d02be291684e386196
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-14 01:29:13 -0500

    test para la division por cero

--- src/test/java/com/jhonatan/junitapp/ejemplo/models/CalculadoraTest.java
+++ src/test/java/com/jhonatan/junitapp/ejemplo/models/CalculadoraTest.java
@@ -1,5 +1,6 @@
 package com.jhonatan.junitapp.ejemplo.models;
 
+import com.jhonatan.junitapp.ejemplo.exceptions.DividirPorCeroException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,5 +29,30 @@ class CalculadoraTest {
         }, () -> {
             assertTrue(calculadora.restar(valor1, valor2) == esperado, () -> "El valor deberia ser " + esperado);
         });
+    }
+
+    @Test
+    void testMultiplicar() {
+        Integer valor1 = 0;
+        Integer valor2 = 3;
+        Calculadora calculadora = new Calculadora();
+        Integer esperado = 3;
+        Integer resultado = calculadora.multiplicar(valor1, valor2);
+        assertAll(() -> {
+            assertEquals(esperado, resultado, () -> "El valor deberia ser " + esperado);
+        }, () -> {
+            assertTrue(calculadora.restar(valor1, valor2) == esperado, () -> "El valor deberia ser " + esperado);
+        });
+    }
+
+    @Test
+    void testDividirException() {
+        Integer valor1 = 6;
+        Integer valor2 = 0;
+        Calculadora calculadora = new Calculadora();
+        DividirPorCeroException dividirPorCeroException = assertThrows(DividirPorCeroException.class, () -> {
+            calculadora.dividir(valor1, valor2);
+        });
+        assertEquals("No se puede dividir entre 0", dividirPorCeroException.getMessage());
     }
 }
