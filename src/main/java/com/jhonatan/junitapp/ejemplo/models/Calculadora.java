659235bdfa4ce16c00b016bf1c8bc970f5382040
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-14 01:28:53 -0500

    clase DividirPorCeroException para validar que no se divida por cero

--- src/main/java/com/jhonatan/junitapp/ejemplo/models/Calculadora.java
+++ src/main/java/com/jhonatan/junitapp/ejemplo/models/Calculadora.java
@@ -1,5 +1,7 @@
 package com.jhonatan.junitapp.ejemplo.models;
 
+import com.jhonatan.junitapp.ejemplo.exceptions.DividirPorCeroException;
+
 public class Calculadora {
 
     public Integer sumar(Integer valor01, Integer valor02) {
@@ -10,8 +12,12 @@ public class Calculadora {
         return valor01 - valor02;
     }
 
-    public Integer dividir(Integer valor01, Integer valor02) {
-        return valor01 / valor02;
+    public Integer dividir(Integer valor01, Integer valor02) throws DividirPorCeroException {
+        try {
+            return valor01 / valor02;
+        } catch (ArithmeticException e) {
+            throw new DividirPorCeroException("No se puede dividir por cero.");
+        }
     }
 
     public Integer multiplicar(Integer valor01, Integer valor02) {
